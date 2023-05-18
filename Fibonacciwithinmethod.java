1b1d37d53844b6c4bd24342506ba1c658cba210b
Ashish Gupta <131584885+atpugashish@users.noreply.github.com>
2023-05-18 13:33:30 +0530

    Add files via upload

--- Fibonacciwithinmethod.java
+++ Fibonacciwithinmethod.java
@@ -2,22 +2,30 @@ package com.Ashish3;
 
 import java.util.Scanner;
 
-public class Fibonacci {
+public class Fibonacciwithinmethod {
     public static void main(String[] args) {
+        fibonacci();
+        fibonacci();
+        fibonacci();
+
+    }
+
+    static void fibonacci() {
+
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the Nth number : ");
         int num = in.nextInt();
+
         int num1 = 0;
         int num2 = 1;
         int nextnum = 0;
-        while (nextnum < num){
-            System.out.println(num1 + " ");
+        while (nextnum < num) {
+            System.out.println(num1 + "   ");
             int num3 = num1 + num2;
             num1 = num2;
             num2 = num3;
             nextnum = nextnum + 1;
+
         }
-
     }
-
 }
